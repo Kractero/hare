f9c0a40d96b27fd569677c6e9c4004a9489d479b
Kractero <kractero@sideroca.com>
2023-09-30 02:04:31 -0700

    dynamic route for tools

---
+++ src/routes/tools/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { tools } from '$lib/tools';
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+    let credentials = false;
+    tools.forEach(tool => {
+        if (tool.name === data.tool.name) {
+            credentials = tool.credentials
+        }
+    })
+</script>
+
+<h1>{data.tool.name}</h1>
+<p>{credentials}</p>
