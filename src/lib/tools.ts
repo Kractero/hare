28e6f339fc26e0104f511125e951c4055aab8c71
Kractero <kractero@sideroca.com>
2023-10-01 01:05:57 -0700

    incorporate a pinger and wheresmywa

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,8 +1,8 @@
 interface Tool {
     name: string;
+    slug?: string;
     description: string;
     category: string;
-    credentials: boolean
 }
 
 /*
@@ -13,14 +13,50 @@ interface Tool {
 export const tools: Array<Tool> = [
     {
         "name": "gotIssues",
+        "slug": "gotissues",
         "description": "An even faster way to answer issues with python.",
-        "category": "cards",
-        "credentials": true,
+        "category": "cards"
     },
     {
         "name": "junkDaJunk",
+        "slug": "junkdajunk",
         "description": "An even faster way to junk cards with python.",
-        "category": "cards",
-        "credentials": true,
+        "category": "cards"
+    },
+    {
+        "name": "Gold Retriever",
+        "slug": "goldretriever",
+        "description": "Check puppet trading cards', total bank, deck value, and more.",
+        "category": "cards"
+    },
+    {
+        "name": "Containerise Rules",
+        "slug": "containerise",
+        "description": "Generate container rules for use with Containerise.",
+        "category": "cards"
+    },
+    {
+        "name": "Login Sheet",
+        "slug": "login",
+        "description": "Check puppet trading cards', total bank, deck value, and more",
+        "category": "cards"
+    },
+    {
+        "name": "Pinger",
+        "slug": "ping",
+        "description": "Ping all your nations, delaying their impending doom.",
+        "category": "cards"
+    },
+    {
+        "name": "Wheres My WA",
+        "slug": "whereswa",
+        "description": "Ping all your nations, delaying their impending doom.",
+        "category": "cards"
+    },
+    {
+        "name": "Rate of Change",
+        "slug": "roc",
+        "description": "Ping all your nations, delaying their impending doom.",
+        "category": "cards"
     }
 ]
