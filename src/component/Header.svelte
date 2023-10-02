fa47839e1fbf19fd1c222b50b9a19f6cb90aa826
Kractero <kractero@sideroca.com>
2023-10-01 23:10:23 -0700

    add modal for configuring inputs

--- src/component/Header.svelte
+++ src/component/Header.svelte
@@ -6,8 +6,8 @@
 
 <header class="flex justify-between w-full mb-32 mt-3">
 	<a href="/" class="flex items-center">
-		<img src="/Train.png" alt="Train" />
-		<h2 class="text-2xl font-bold tracking-tight ml-2">Kominkan</h2>
+		<img src="/Computer.png" alt="Computer" />
+		<h2 class="text-2xl font-bold tracking-tight ml-2">Hare</h2>
 	</a>
 	<div class="flex gap-4">
 		<a href="https://discord.gg/yn5a4p9" target="_blank" rel="noreferrer noopener">
