de6f53995441d625ed7cc4a00dc189b276e7a9b1
Kractero <kractero@sideroca.com>
2023-10-12 15:06:12 -0700

    prerender cards resources

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,14 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 
 		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (path === '/not-found') {
+					return;
+				}
+			},
+			entries: ["/", "/config", "/disclaimer", "/guides", "/resources"]
+		}
 	}
 };
 
