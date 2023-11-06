6eaacf8147ce9caeafe7538c150d6f6db5ded969
Kractero <kractero@sideroca.com>
2023-11-05 23:31:35 -0800

    Add undocumented changelog page

---
+++ src/lib/data/changelog.ts
@@ -0,0 +1,91 @@
+export const changelog = [
+    {
+        "date": "2023-11-06",
+        "changes": {
+            "fixes": [
+                [   
+                    "JunkDaJunk",
+                    "Cast the region returned from the NationStates API into a string. I believe this will avoid regions like 0000 being caught in the whitelist."
+                ],              
+                [   
+                    "Legendary Tracker",
+                    "Fixed date interpolation in the subtitle."
+                ]
+            ]
+        }
+    },
+    {
+        "date": "2023-11-05",
+        "changes": {
+            "features": [
+                [   
+                    "Finder, Gold Retriever, JunkDaJunk, Pinger",
+                    "Added support for nation,password formatting, identical to the behavior of gotIssues."
+                ]
+            ],
+        }
+    },
+    {
+        "date": "2023-11-04",
+        "changes": {
+            "features": [
+                [
+                    "Statistics",
+                    "Added name mappings to census number values (IE: 3 is Population) to the terminal output."
+                ]
+            ],
+            "fixes": [
+                [
+                    "Flag Manager",
+                    "Include error handling to prevent tool from stopping prematurely on error."
+                ]
+            ]
+        }
+    },
+    {
+        "date": "2023-11-02",
+        "changes": {
+            "features": [
+                [
+                    "Signal",
+                    "NEW - A new tool that works with card queries. Given a list of card ids, you can get the cards from that id list that are not present in the provided collections/decks/asks or bids."
+                ],
+                [
+                    "Pinger",
+                    "Pinger now implements an iframe that allows for restoring nations."
+                ],
+                [
+                    "Statistics",
+                    "Backend implementation for name mappings to census number values."
+                ]
+            ],
+            "fixes": [
+                [
+                    "Guides - Cardtainers",
+                    "Add links where they make sense and fix broken pre element at the bottom."
+                ]
+            ]
+        }
+    },
+    {
+        "date": "2023-11-04",
+        "changes": {
+            "features": [
+                [
+                    "Statistics",
+                    "NEW - A new tool that takes a list of nations, then gets statistics like the mean, average, and total for the provided census stat categories."
+                ],
+                [
+                    "Config",
+                    "The config now displays toasts when the submit button is clicked, to indicate which config values have changed."
+                ]
+            ],
+            "fixes": [
+                [
+                    "All",
+                    "Internal adjustments to tool components."
+                ]
+            ]
+        }
+    }
+]
---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import ToolContent from "$lib/component/ToolContent.svelte";
+	import { changelog } from "$lib/data/changelog";
+</script>
+<ToolContent toolTitle="Changelog" caption="A Hare Changelog" />
+
+<div class="lg:w-[1024px] lg:max-w-5xl flex flex-col justify-between gap-8 break-normal mb-4">
+    {#each changelog as day}
+        <div>
+            <h2 class="font-bold text-3xl mb-2">{day.date}</h2>
+            {#if day.changes.features}
+                <p class="mb-2 font-semibold text-xl">Features</p>
+
+                {#each day.changes.features as feature}
+                    <p class="mb-1">{feature[0]}</p>
+                    <ul class="list-disc ml-4 mb-4 text-sm">
+                        <li>{feature[1]}</li>
+                    </ul>
+                {/each}
+            {/if}
+            {#if day.changes.fixes}
+                <p class="mb-2 font-semibold text-xl">Fixes</p>
+
+                {#each day.changes.fixes as fix}
+                    <p class="mb-1">{fix[0]}</p>
+                    <ul class="list-disc ml-4 mb-4 text-sm">
+                        <li>{fix[1]}</li>
+                    </ul>
+                {/each}
+            {/if}
+        </div>
+    {/each}
+</div>
