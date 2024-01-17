6d2daa6a7e5ac2a9dbabd10f8dac05290b070ef1
Kractero <kractero@sideroca.com>
2024-01-17 11:17:09 -0700

    Convert changelog to md for infinitely less overhead

--- src/routes/changelog/+page.svelte
+++ src/routes/changelog/+page.svelte
@@ -1,37 +1,8 @@
-<script>
-	import ToolContent from "$lib/component/ToolContent.svelte";
-	import { changelog } from "$lib/data/changelog";
+<script lang="ts">
+    // @ts-ignore
+    import Changelog from "./Changelog.md"
 </script>
-<ToolContent toolTitle="Changelog" caption="A Hare Changelog" />
 
-<div class="lg:w-[1024px] lg:max-w-5xl flex flex-col justify-between gap-8 break-normal mb-4">
-    {#each changelog as day}
-        <div>
-            <h2 class="font-bold text-3xl mb-2">{day.date}</h2>
-            {#if day.changes.features}
-                <p class="mb-2 font-semibold text-xl">Features</p>
-
-                {#each day.changes.features as feature}
-                    <p class="mb-1">{feature[0]}</p>
-                    <ul class="list-disc ml-4 mb-4 text-sm">
-                        {#each feature.slice(1) as changes}
-                            <li>{changes}</li>
-                        {/each}
-                    </ul>
-                {/each}
-            {/if}
-            {#if day.changes.fixes}
-                <p class="mb-2 font-semibold text-xl">Fixes</p>
-
-                {#each day.changes.fixes as fix}
-                    <p class="mb-1">{fix[0]}</p>
-                    <ul class="list-disc ml-4 mb-4 text-sm">
-                        {#each fix.slice(1) as changes}
-                            <li>{changes}</li>
-                        {/each}
-                    </ul>
-                {/each}
-            {/if}
-        </div>
-    {/each}
-</div>
+<article class="prose dark:prose-invert max-w-lg md:max-w-3xl lg:max-w-5xl p-4 mb-24">
+    <Changelog />
+</article>
