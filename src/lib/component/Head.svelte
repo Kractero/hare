031999e221ddf7e8de2774ddc7131a5206f8eed6
Kractero <kractero@sideroca.com>
2024-03-11 21:32:04 -0700

    extract out log code, remove commented changelog in header

--- src/lib/component/Head.svelte
+++ src/lib/component/Head.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     export let title: string;
     export let description: string;
+    import { PUBLIC_LOGGING_SCRIPT, PUBLIC_LOG_ID } from '$env/static/public';
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-    <script async src="https://nakiri.vercel.app/script.js" data-website-id="c3f1d5ce-9d0d-4a17-902f-41e13b841005"></script>
+    <script async src={PUBLIC_LOGGING_SCRIPT} data-website-id={PUBLIC_LOG_ID}></script>
 </svelte:head>
--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -4,10 +4,6 @@
 		<h2 class="text-2xl font-bold tracking-tight ml-2">Hare</h2>
 	</a>
 	<div class="flex gap-1 sm:gap-4">
-		<!-- <a class="flex items-center gap-2" href={`/config`}>
-			<p class="hidden sm:block sm:text-base md:text-xl font-semibold">Changelog</p>
-			<img src="/Newspaper.png" alt="Settings" />
-		</a> -->
 		<a class="flex items-center gap-2" href={`/config`}>
 			<p class="text-xl font-semibold">Settings</p>
 			<img src="/Settings.png" alt="Settings" />
