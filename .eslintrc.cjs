e5b37b6360f15df0db71df542eba32ad8c8a9957
Kractero <kractero@sideroca.com>
2024-08-21 00:55:14 -0700

    environment update (packages, nix, prettier, eslint)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
-		'prettier'
+		'prettier',
+		"plugin:tailwindcss/recommended"
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
