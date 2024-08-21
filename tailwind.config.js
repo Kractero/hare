e4efe9ba3ea52cc0621e0a263c2437ae03c3466d
Kractero <kractero@sideroca.com>
2024-08-21 01:09:08 -0700

    install shadcn-svelte and lucide-svelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,55 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
 	darkMode: ["class"],
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	safelist: ["dark"],
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px"
+			}
+		},
 		extend: {
 			colors: {
-				background: 'rgb(var(--color-primary) / <alpha-value>)',
+				border: "hsl(var(--border) / <alpha-value>)",
+				input: "hsl(var(--input) / <alpha-value>)",
+				ring: "hsl(var(--ring) / <alpha-value>)",
+				background: "hsl(var(--background) / <alpha-value>)",
+				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				primary: {
+					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card) / <alpha-value>)",
+					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+				},
+
+				backgroundold: 'rgb(var(--color-primary) / <alpha-value>)',
 				content: 'rgb(var(--color-secondary) / <alpha-value>)',
 				tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
 				textprimary: 'rgb(var(--text-primary) / <alpha-value>)',
@@ -17,7 +61,13 @@ export default {
 				herobuttonshover: 'rgb(var(--herobuttons-hover) / <alpha-value>)',
 				buttons: 'rgb(var(--buttons) / <alpha-value>)'
 			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
+			},
 			fontFamily: {
+				sans: [...fontFamily.sans],
 				notosans: ['Noto Sans', 'sans-serif']
 			}
 		}
@@ -27,3 +77,5 @@ export default {
 		'prettier-plugin-tailwindcss'
 	]
 };
+
+export default config;
