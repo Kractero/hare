32a1ad588d5847cd94b4d2ba7d8034d69b31540e
Kractero <kractero@sideroca.com>
2024-08-21 17:42:00 -0700

    approvals: ui, add a sharable select component

---
+++ src/lib/components/FormSelect.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import * as Select from '$lib/components/ui/select'
+
+	import Label from './ui/label/label.svelte'
+
+	export let label: string
+	export let subTitle: string = ''
+	export let id: string
+	export let items: string[]
+	export let bindValue: any
+</script>
+
+<div class={`flex max-w-lg items-center justify-between gap-4`}>
+	<Label class={'w-24'} for={id}
+		>{label}
+		{#if subTitle}
+			<span class="text-xs">{subTitle}</span>
+		{/if}</Label
+	>
+	<Select.Root
+		onSelectedChange={v => {
+			bindValue = v && v.value
+		}}
+	>
+		<Select.Trigger class="flex-1">
+			<Select.Value placeholder={items[0]} />
+		</Select.Trigger>
+		<Select.Content class="text-left">
+			{#each items as item}
+				<Select.Item value={item}>{item}</Select.Item>
+			{/each}
+		</Select.Content>
+	</Select.Root>
+</div>
--- src/routes/tools/approval/+page.svelte
+++ src/routes/tools/approval/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte'
-	import { parseXML } from '$lib/helpers/utils'
-	import Terminal from '$lib/component/Terminal.svelte'
-	import Buttons from '$lib/component/Buttons.svelte'
-	import Input from '$lib/component/Input.svelte'
-	import Select from '$lib/component/Select.svelte'
-	const abortController = new AbortController()
-	import { pushHistory } from '$lib/helpers/utils'
-	import ToolContent from '$lib/component/ToolContent.svelte'
 	import { page } from '$app/stores'
+	import Buttons from '$lib/components/Buttons.svelte'
+	import UserAgent from '$lib/components/formFields/UserAgent.svelte'
+	import FormInput from '$lib/components/FormInput.svelte'
+	import FormSelect from '$lib/components/FormSelect.svelte'
+	import Terminal from '$lib/components/Terminal.svelte'
+	import ToolContent from '$lib/components/ToolContent.svelte'
+	import { pushHistory } from '$lib/helpers/navigation'
+	import { parseXML } from '$lib/helpers/parser'
+
+	const abortController = new AbortController()
+
 	let progress = ''
 	let content = ''
 	let downloadable = false
@@ -36,9 +39,7 @@
 		stoppable = true
 		stopped = false
 		let councilID = 1
-		if (council === 'Security Council') {
-			councilID = 2
-		}
+		if (council === 'Security Council') councilID = 2
 		progress = `<p>Retrieving list of esteemed delegates...</p>`
 		let delegatesXML = await parseXML(
 			`https://${localStorage.getItem('connectionUrl') || 'www'}.nationstates.net/cgi-bin/api.cgi?wa=1&q=delegates`,
@@ -50,9 +51,15 @@
 			`https://${localStorage.getItem('connectionUrl') || 'www'}.nationstates.net/cgi-bin/api.cgi?wa=${councilID}&q=proposals`,
 			main
 		)
-		const proposal = proposalXML.WA.PROPOSALS.PROPOSAL.filter(
-			(proposal: { [x: string]: string }) => proposal['@_id'] === proposalid
-		)[0]
+		if (!proposalXML.WA.PROPOSALS.PROPOSAL) {
+			progress += `No proposals currently in the ${council}.`
+			return
+		}
+		const proposal = Array.isArray(proposalXML.WA.PROPOSALS.PROPOSAL)
+			? proposalXML.WA.PROPOSALS.PROPOSAL.filter(
+					(proposal: { [x: string]: string }) => proposal['@_id'] === proposalid
+				)[0]
+			: proposalXML.WA.PROPOSALS.PROPOSAL['@_id'] === proposalid
 		if (!proposal) {
 			progress += `<p class="text-red-400">No proposal found matching ${proposalid} in the ${council}.</p>`
 			stoppable = false
@@ -91,11 +98,24 @@
 	caption="Specify a proposal and get all delegates that are not approving it."
 />
 
-<div class="lg:w-[1024px] lg:max-w-5xl flex flex-col lg:flex-row gap-8 break-normal">
+<div
+	class="flex flex-col justify-between gap-8 break-normal lg:w-[1024px] lg:max-w-5xl lg:flex-row"
+>
 	<form on:submit|preventDefault={() => approvals()} class="flex flex-col gap-8">
-		<Input text="User Agent" bind:bindValue={main} forValue="main" required={true} />
-		<Select name="Council" mode={council} options={['General Assembly', 'Security Council']} />
-		<Input text="Proposal ID" bind:bindValue={proposalid} forValue="proposalID" required={true} />
+		<UserAgent bind:main />
+		<FormSelect
+			id="council"
+			label="Council"
+			bind:bindValue={council}
+			items={['General Assembly', 'Security Council']}
+		/>
+		<FormInput
+			bind:bindValue={proposalid}
+			id="proposalID"
+			label="Proposal ID"
+			placeholder="311"
+			required={true}
+		/>
 		<Buttons downloadButton={true} bind:downloadable bind:content type="html" name="Approvals" />
 	</form>
 	<Terminal bind:progress />
