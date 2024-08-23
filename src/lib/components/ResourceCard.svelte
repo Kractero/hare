f113f11fc4b608160a46fbc0a587134b18502301
Kractero <kractero@sideroca.com>
2024-08-22 23:13:54 -0700

    adjust html

--- src/lib/components/ResourceCard.svelte
+++ src/lib/components/ResourceCard.svelte
@@ -18,23 +18,17 @@
 <article
 	class="flex max-w-full flex-col gap-4 rounded-xl p-4 hover:shadow-lg dark:bg-neutral-800 md:p-6"
 >
-	<div class="-mb-1 flex flex-wrap items-center gap-2">
-		<a href="/monitoring" class="max-w-full text-sm text-muted-foreground"
-			>{category || 'NationStates'}</a
-		>
-	</div>
+	<p class="-mb-1 flex flex-wrap items-center gap-2">{category}</p>
 	<a href={url} target="_blank">
 		<h2 class="break-words text-3xl font-bold">{name}</h2>
 	</a>
-	<a href={authorUrl} target="_blank">
-		<p class="text-base font-light text-secondary-foreground">{author}</p>
+	<a class="text-base font-light text-secondary-foreground" href={authorUrl} target="_blank"
+		>{author}
 	</a>
-	<div class="text-sm text-muted-foreground">last updated {timeAgo(lastUpdate)}</div>
-	<div class="break-words">
-		<div>
-			{description}
-		</div>
-	</div>
+	<p class="text-sm text-muted-foreground">last updated {timeAgo(lastUpdate)}</p>
+	<p class="break-words">
+		{description}
+	</p>
 	<div class="mb-auto">
 		<div class="flex flex-wrap gap-2">
 			{#each keywords as word}
@@ -45,6 +39,6 @@
 		</div>
 	</div>
 	<div class="flex w-full flex-col justify-between min-[460px]:flex-row sm:items-end sm:gap-6">
-		<div class="text-sm text-muted-foreground">created {timeAgo(created)}</div>
+		<p class="text-sm text-muted-foreground">created {timeAgo(created)}</p>
 	</div>
 </article>
