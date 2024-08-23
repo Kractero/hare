11a21510a5deac33d5b042728b4e27ec5a1771ca
Kractero <kractero@sideroca.com>
2024-08-22 22:54:47 -0700

    home page

---
+++ src/lib/components/Tool.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let slug = ''
+	export let tool = ''
+	export let description = ''
+</script>
+
+<a
+	href={slug}
+	target={slug.includes('https') ? '_blank' : '_self'}
+	rel="noreferrer noopener"
+	class="mb-16 flex w-60 flex-col items-start rounded-sm border border-black bg-content p-4 text-cardprimarytext transition duration-200 hover:scale-105 hover:shadow-lg"
+>
+	<div class="font-semi-bold flex items-center text-xl">
+		<img class="mr-2" alt={tool} src={`/${tool}.png`} />
+		{tool}
+	</div>
+	<p class="mt-2 text-sm text-cardsecondarytext">{description}</p>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,68 @@
 <script>
-	import Head from '$lib/component/Head.svelte';
-	import Linkbar from '$lib/component/Linkbar.svelte';
-	import { otherHareTools, tools } from '$lib/data/tools';
-	import Tool from '$lib/component/Tool.svelte';
-	let searchTerm = '';
+	import Head from '$lib/components/Head.svelte'
+	import Linkbar from '$lib/components/Linkbar.svelte'
+	import Tool from '$lib/components/Tool.svelte'
+	import externalTools from '$lib/data/external-tools.json'
+	import tools from '$lib/data/tools.json'
+
+	let searchTerm = ''
 </script>
 
-<Head title={"Hare"} description={"Humble Automated Runner for Executing nationstates scripts. A central location to run NationStates scripts."} />
+<Head
+	title={'Hare'}
+	description={'Humble Automated Runner for Executing nationstates scripts. A central location to run NationStates scripts.'}
+/>
 
-<div class="lg:w-[1024px] md:max-w-5xl mb-12">
-	<h1 class="text-textprimary font-bold tracking-wide text-4xl mb-4">Hare (ハレ)</h1>
-	<p class="font-extralight tracking-tight">A central location for NationStates scripts.</p>
-	<Linkbar />
-	<div class="flex gap-2">
-		<a href="https://discord.gg/yn5a4p9" target="_blank" rel="noreferrer noopener">
-			<img src="/Discord.png" alt="discord" />
-		</a>
-		<a href="https://github.com/Kractero/hare" target="_blank" rel="noreferrer noopener">
-			<img src="/Github.png" alt="Github" />
-		</a>
+<div class="mb-12 grid grid-cols-12 items-center justify-between md:max-w-5xl lg:w-[1024px]">
+	<div class="col-span-full flex flex-col gap-2 lg:col-span-6">
+		<h1 class="mb-4 text-center text-5xl font-bold tracking-tight text-textprimary lg:text-left">
+			A central location for NationStates scripts.
+		</h1>
+		<p class="text-center font-extralight tracking-tight lg:text-left">
+			The sweetest soda is the drink of intellectuals.
+		</p>
+		<Linkbar />
+	</div>
+	<div class="col-span-full mx-auto max-lg:hidden lg:col-span-6">
+		<img src="hare2.jpg" alt="hare" />
 	</div>
 </div>
 
-<p class="mb-2">Search</p>
+<!-- <p class="mb-2">Search</p>
 <input
 	bind:value={searchTerm}
-	class="text-black p-1 w-full rounded-md border border-black dark:border-none mb-16 dark:bg-neutral-200"
-/>
+	class="mb-16 w-full rounded-md border border-black p-1 text-black dark:border-none dark:bg-neutral-200"
+/> -->
 
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-	{#each tools.filter((project) => {
+	{#each tools.filter(project => {
 		if (searchTerm) {
 			return project.description.toLowerCase().includes(searchTerm.toLowerCase()) || project.name
 					.toLowerCase()
-					.includes(searchTerm.toLowerCase());
+					.includes(searchTerm.toLowerCase())
 		} else {
-			return project;
+			return project
 		}
 	}) as tool}
 		<Tool slug={`/tools/${tool.slug}`} tool={tool.name} description={tool.description} />
 	{/each}
 </div>
 
-<h2 class="text-2xl font-semibold mb-4">Non-Scripts</h2>
+<h2 class="mb-4 text-2xl font-semibold">Non-Scripts</h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-	{#each otherHareTools.filter((project) => {
+	{#each externalTools.filter(project => {
 		if (searchTerm) {
 			return project.description.toLowerCase().includes(searchTerm.toLowerCase()) || project.name
 					.toLowerCase()
-					.includes(searchTerm.toLowerCase());
+					.includes(searchTerm.toLowerCase())
 		} else {
-			return project;
+			return project
 		}
 	}) as tool}
-		<Tool slug={tool.slug.includes('https') ? tool.slug : `/resources/${tool.slug}`} tool={tool.name} description={tool.description} />
+		<Tool
+			slug={tool.slug.includes('https') ? tool.slug : `/resources/${tool.slug}`}
+			tool={tool.name}
+			description={tool.description}
+		/>
 	{/each}
 </div>
