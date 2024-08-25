15643ebcbfca63b9b06862da69d1a5bb186e413d
Kractero <kractero@sideroca.com>
2024-08-24 17:04:09 -0700

    use gif fit better

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,20 +2,21 @@
 	import { page } from '$app/stores'
 </script>
 
-<h1 class="max-w-lg text-xl font-medium text-destructive">Ugh. I really tried. I'm sorry.</h1>
-{#if $page?.error}
-	<div class="my-4">
-		{#if $page?.status}
-			<p class="max-w-lg text-base font-medium text-destructive">Page status: {$page?.status}</p>
-		{/if}
-		{#if $page?.error?.message}
-			<p class="max-w-lg text-base font-medium text-destructive">
-				Error message: {$page?.error?.message}
-			</p>
-		{/if}
-	</div>
-{/if}
+<div class="max:w-[496px] flex flex-col items-center gap-4 lg:w-[1024px] lg:max-w-5xl">
+	<h1 class="max-w-lg text-xl font-medium text-destructive">Ugh. I really tried. I'm sorry.</h1>
 
-<video controls autoplay loop>
-	<source src="/error.mp4" type="video/mp4" />
-</video>
+	<img class="mx-auto w-full max-w-[496px] lg:max-w-max" alt="error" src="/hare.gif" />
+
+	{#if $page?.error}
+		<div class="my-4">
+			{#if $page?.status}
+				<p class="max-w-lg text-base font-medium text-destructive">Page status: {$page?.status}</p>
+			{/if}
+			{#if $page?.error?.message}
+				<p class="max-w-lg text-base font-medium text-destructive">
+					Error message: {$page?.error?.message}
+				</p>
+			{/if}
+		</div>
+	{/if}
+</div>
