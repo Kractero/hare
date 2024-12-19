c9492c48c16a64b6b2c12e0d971e256a5655f2d6
Kractero <kractero@sideroca.com>
2024-12-19 08:16:34 -0700

    test transfer

---
+++ src/routes/tools/auction/+page.svelte
@@ -0,0 +1,226 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte'
+	import { page } from '$app/state'
+	import Buttons from '$lib/components/Buttons.svelte'
+	import UserAgent from '$lib/components/formFields/UserAgent.svelte'
+	import FormInput from '$lib/components/FormInput.svelte'
+	import FormSelect from '$lib/components/FormSelect.svelte'
+	import FormTextArea from '$lib/components/FormTextArea.svelte'
+	import Terminal from '$lib/components/Terminal.svelte'
+	import ToolContent from '$lib/components/ToolContent.svelte'
+	import { parseXML } from '$lib/helpers/parser'
+	import { checkUserAgent, pushHistory } from '$lib/helpers/utils'
+
+	const abortController = new AbortController()
+
+	let domain = ''
+	let progress = $state('')
+	let downloadable = $state(false)
+	let stoppable = $state(false)
+	let stopped = $state(false)
+	let main = $state('')
+	let puppets = $state('')
+	let content = $state('')
+	let transfer = $state('10')
+	let mode = $state('Transfer')
+	let mainTransfer = $state('')
+	let transferCards = $state('')
+	let errors: Array<{ field: string | number; message: string }> = $state([])
+
+	onMount(() => {
+		domain = `https://${localStorage.getItem('connectionUrl') || 'www'}.nationstates.net`
+		main = page.url.searchParams.get('main') || (localStorage.getItem('main') as string) || ''
+		puppets = (localStorage.getItem('puppets') as string) || ''
+		transfer = page.url.searchParams.get('transfer') || (localStorage.getItem('transferBank') as string) || '10'
+		mode = page.url.searchParams.get('mode') || (localStorage.getItem('transferMode') as string) || 'Transfer'
+	})
+
+	onDestroy(() => abortController.abort())
+
+	async function onSubmit(e: Event) {
+		e.preventDefault()
+		pushHistory(`?main=${main}&transfer=${transfer}&mode=${mode}`)
+		errors = checkUserAgent('main')
+		if (errors.length > 0) return
+		downloadable = false
+		stoppable = true
+		stopped = false
+		progress = '<p>Initiating Transfer List...</p>'
+		let puppetList = puppets.split('\n')
+		let count = 0
+		let bank = 0
+		content = ''
+		const findSplit = transferCards.split('\n').map(matcher => matcher.split(','))
+		const transferCounts: { [key: string]: any } = {}
+
+		if (mode === 'Bids' || mode === 'Asks') {
+			let counter = 0
+			for (const card of findSplit) {
+				const [id, season] = card
+				const singleLink = `https://www.nationstates.net/nation=${mainTransfer}/page=deck/card=${id}/season=${season}?mode=${mode === 'Bids' ? 'bid' : 'ask'}&amount=${transfer}`
+				counter++
+				content += `<tr><td><p>${counter}</p></td><td><p><a target="_blank" href="${singleLink}">Link to ${mode === 'Bids' ? 'bid' : 'ask'}</a></p></td></tr>\n`
+			}
+
+			progress += `${mode === 'Bids' ? 'Bid' : 'Ask'} links generated`
+		}
+
+		if (mode === 'Transfer') {
+			const deckInfo = await parseXML(`${domain}/cgi-bin/api.cgi?nationname=${mainTransfer}&q=cards+deck+info`, main)
+			let cards = deckInfo.CARDS.DECK.CARD
+			cards = cards ? (Array.isArray(cards) ? cards : [cards]) : []
+
+			if (cards.length > 0) {
+				for (const card of cards) {
+					const id = card.CARDID
+					const season = card.SEASON
+
+					const matchingEntries = findSplit.filter(match => match[0] === String(id) && match[1] === String(season))
+					if (matchingEntries.length > 0) {
+						if (!transferCounts[id]) {
+							transferCounts[id] = { count: 0, season, id }
+						}
+						transferCounts[id].count++
+					}
+				}
+
+				for (const [id, { count, season }] of Object.entries(transferCounts)) {
+					progress += `<p class="text-blue-400">${mainTransfer} has ${count} copies of card ID ${id}, season ${season}</p>`
+				}
+			}
+			let puppetTransfer: { name: string; transfer: number }[] = []
+			let totalTransferrable = 0
+
+			for (let i = 0; i < puppetList.length; i++) {
+				let nation = puppetList[i]
+				if (abortController.signal.aborted || stopped) {
+					break
+				}
+				try {
+					progress += `<p>Processing ${nation} ${i + 1}/${puppetList.length}</p>`
+					const deckInfo = await parseXML(`${domain}/cgi-bin/api.cgi?nationname=${nation}&q=cards+deck+info`, main)
+					let nationalBank = deckInfo.CARDS.INFO.BANK
+
+					if (nationalBank >= Number(transfer)) {
+						let cardsTransferable = Math.floor(nationalBank / Number(transfer))
+						progress += `<p class="text-green-400">${nation} can transfer ${cardsTransferable} cards!</p>`
+						puppetTransfer.push({ name: nation, transfer: cardsTransferable })
+						totalTransferrable += cardsTransferable
+						count++
+						bank += nationalBank
+					} else {
+						progress += `<p class="text-yellow-400">${nation} cannot afford any cards (Bank: ${nationalBank}).</p>`
+					}
+				} catch (err) {
+					progress += `<p class="text-red-400">Error processing ${nation} with ${err}</p>`
+				}
+			}
+
+			let askQuantity = 0
+			let remainingTransferable = totalTransferrable
+
+			for (const { count, id, season } of Object.values(transferCounts)) {
+				while (remainingTransferable > 0 && count > 0) {
+					progress += `<p>${askQuantity + 1} Generated bid link for card ID ${id}, season ${season} to ${name}</p>`
+					const singleLink = `https://www.nationstates.net/nation=${mainTransfer}/page=deck/card=${id}/season=${season}?mode=ask&amount=${transfer}`
+					askQuantity++
+
+					content += `<tr><td><p>${askQuantity}</p></td><td><p><a target="_blank" href="${singleLink}">Link to Ask</a></p></td></tr>\n`
+					remainingTransferable--
+				}
+
+				if (remainingTransferable <= 0) {
+					break
+				}
+			}
+
+			let bidQuantity = 0
+			for (let i = 0; i < puppetTransfer.length; i++) {
+				let { name, transfer: cardsTransferable } = puppetTransfer[i]
+
+				// for (const [id, { count, season }] of Object.entries(transferCounts)) {
+				// 	if (cardsTransferable > 0 && transferCounts[id].count > 0) {
+				// 		progress += `<p class="text-gray-400">Generated initial transfer link for card ID ${id}, season ${season} to ${name}</p>`
+				// 		const singleLink = `https://www.nationstates.net/nation=${name}/page=deck/card=${id}/season=${season}?mode=bid&amount=${transfer}`
+				// 		openNewLinkArr = [...openNewLinkArr, singleLink]
+
+				// 		content += `<tr><td><p>${
+				// 			bidQuantity + 1
+				// 		}</p></td><td><p><a target="_blank" href="${singleLink}">Link to Issue</a></p></td></tr>\n`
+
+				// 		bidQuantity = bidQuantity + 1
+				// 		transferCounts[id].count--
+				// 		cardsTransferable--
+				// 	}
+				// 	if (cardsTransferable <= 0) break
+				// }
+
+				for (const [id, { count, season }] of Object.entries(transferCounts)) {
+					while (cardsTransferable > 0 && transferCounts[id].count > 0) {
+						progress += `<p>${bidQuantity + 1} Generated bid link for card ID ${id}, season ${season} to ${name}</p>`
+						const singleLink = `https://www.nationstates.net/nation=${name}/page=deck/card=${id}/season=${season}?mode=bid&amount=${transfer}`
+						bidQuantity++
+
+						content += `<tr><td><p>${
+							bidQuantity
+						}</p></td><td><p><a target="_blank" href="${singleLink}">Link to Bid</a></p></td></tr>\n`
+
+						transferCounts[id].count--
+						cardsTransferable--
+					}
+					if (cardsTransferable <= 0) break
+				}
+			}
+
+			progress += `${askQuantity} main ask links generated, ${bidQuantity} puppet bid links generated`
+		}
+
+		downloadable = true
+		stoppable = false
+	}
+</script>
+
+<ToolContent
+	toolTitle="Auction"
+	caption="Temp name. A testing transfer tool."
+	additional={`<p class="mb-2">
+	Mode Transfer will first sum the total amount of transfer cards located on the main nation. It will then figure out how many times
+	the provided puppets can transfer under the amount. It will then generate bid links equal to that amount, then ask links decrementing
+	until all possible transfers have links.
+</p>
+<p class="mb-2">
+	Mode Bid/Ask will just place one bid or ask with the amount from the main nation on each card.
+</p>
+<p class="mb-2">
+	Requires the
+	<a class="underline" href="https://hare.kractero.com/auction.user.js" target="_blank" rel="noreferrer noopener">
+		hare auction userscript
+	</a>
+	to be useful.
+</p>
+<p class="mb-2">
+	Accepts id,season
+</p>`} />
+
+<div class="flex flex-col gap-8 break-normal lg:w-[1024px] lg:max-w-5xl lg:flex-row">
+	<form onsubmit={onSubmit} class="flex flex-col gap-8">
+		<UserAgent bind:main bind:errors />
+		{#if mode === 'Transfer'}
+			<FormTextArea bind:bindValue={puppets} id="puppets" label="Puppets" required={false} />
+		{/if}
+		<FormTextArea bind:bindValue={transferCards} label={'Cards'} id="transferCards" />
+		<FormSelect id="mode" label="Mode" bind:bindValue={mode} items={['Transfer', 'Bids', 'Asks']} />
+		<FormInput label="Main Nation" bind:bindValue={mainTransfer} id="mainTransfer" required={true} />
+		<FormInput label="Amount" bind:bindValue={transfer} id="amount" required={true} />
+		<Buttons
+			downloadButton={true}
+			bind:downloadable
+			bind:content
+			name="Auction"
+			stopButton={true}
+			bind:stoppable
+			bind:stopped>
+		</Buttons>
+	</form>
+	<Terminal bind:progress />
+</div>
---
+++ static/auction.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        Hare Auction
+// @namespace   Kractero
+// @match       https://www.nationstates.net/*page=deck/card=*/season=*
+// @grant       window.close
+// @version     1.0
+// @author      Kractero
+// ==/UserScript==
+
+;(function () {
+	'use strict'
+
+	const referrer = document.referrer ? new URL(document.referrer) : null
+	const referrerMode = referrer ? referrer.searchParams.get('mode') : null
+	if (referrerMode) {
+		if (document.querySelector('.info') && document.querySelector('.info').textContent.includes('has been lodged'))
+			window.close()
+	}
+
+	const uuurl = new URL(window.location.href)
+	const amount = uuurl.searchParams.get('amount')
+	const mode = uuurl.searchParams.get('mode')
+
+	if (mode === 'ask') {
+		document.querySelector('#cardauctionoffertable').classList.add('mode-sell')
+		if (amount && Number(amount) > 0) {
+			document.querySelector('input.auctionbid[name="auction_ask"]').value = amount
+		}
+		document.querySelectorAll('#cardauctionoffertable button')[0].focus()
+	}
+
+	if (mode === 'bid') {
+		document.querySelector('#cardauctionoffertable').classList.add('mode-buy')
+		if (amount && Number(amount) > 0) {
+			document.querySelector('input.auctionbid[name="auction_bid"]').value = amount
+		}
+		document.querySelectorAll('#cardauctionoffertable button')[1].focus()
+	}
+})()
