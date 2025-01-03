38820301fdb7b08cee8d08b07055e64ec4311735
Kractero <kractero@sideroca.com>
2025-01-02 22:42:30 -0700

    update

--- static/inscription.user.js
+++ static/inscription.user.js
@@ -1,11 +1,17 @@
 // ==UserScript==
 // @name        Inscribe Button Focuser
 // @match       https://*.nationstates.net/*page=deck/card=*
-// @grant       none
-// @version     1.0
+// @grant       window.close
+// @version     1.1
 // @author      Kractero
 // @description Focus inscribe button
 // ==/UserScript==
+
+if (document.querySelector('.info')) {
+	if (document.querySelector('.info').textContent.includes('Card inscribed!')) {
+		window.close()
+	}
+}
 
 if (document.querySelector("button[name='confirm_modify_my_card']"))
 	document.querySelector("button[name='confirm_modify_my_card']").focus()
