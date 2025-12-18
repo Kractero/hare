6794fc989e1c086c8d19b9ba1810cd6a20682369
Kractero <nationstates@kractero.com>
2025-12-18 03:08:41 -0800

    GI: only output no packs if mode is only packs, not both

--- src/routes/tools/gotissues/+page.svelte
+++ src/routes/tools/gotissues/+page.svelte
@@ -134,7 +134,7 @@
 					packCount = packCount === 'All' ? '9' : packCount
 					const packsToOpen = Math.min(packs - Number(minPack), Number(packCount))
 					if (packs === 0) {
-						progress = [...progress, { text: `${nation} has no packs, skipping!`, color: 'blue' }]
+						if (mode === 'Packs') progress = [...progress, { text: `${nation} has no packs, skipping!`, color: 'blue' }]
 						continue
 					}
 					if (packs >= Number(minPack)) {
