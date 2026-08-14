2dfa63c59195c91c4a165dffee75fbd84741f76d
Kractero <nationstates@kractero.com>
2026-08-13 17:01:09 -0700

    should be in previous commit

--- src/lib/helpers/builders.ts
+++ src/lib/helpers/builders.ts
@@ -1,11 +1,11 @@
 const textFiles = ['Transfer', 'Deck', 'Containerise']
 
-export function handleDownload(name: string, files: string[] | string) {
+export function handleDownload(name: string, files: string[] | string, type = 'html') {
 	if (Array.isArray(files)) {
 		downloadBlob(new Blob([files[0]], { type: 'text/txt' }), 'Containerise (Nation).txt')
 		downloadBlob(new Blob([files[1]], { type: 'text/txt' }), 'Containerise (Container).txt')
 	} else {
-		const extension = textFiles.includes(name) ? 'txt' : 'html'
+		const extension = textFiles.includes(name) ? 'txt' : type
 		downloadBlob(new Blob([files], { type: extension }), `${name}.${extension}`)
 	}
 }
